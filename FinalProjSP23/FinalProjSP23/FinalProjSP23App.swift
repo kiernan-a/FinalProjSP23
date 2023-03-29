9db9d49a8cba18658aa7ca49120527f5be4356b6
Kiernan Almand <97690836+kiernan-a@users.noreply.github.com>
2023-03-29 18:20:54 -0400

    first commit

---
+++ FinalProjSP23/FinalProjSP23/FinalProjSP23App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjSP23App.swift
+//  FinalProjSP23
+//
+//  Created by Kiernan Almand on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjSP23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
